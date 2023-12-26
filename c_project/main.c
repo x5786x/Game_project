ff9742e7302d6d1479e70ffc8b181fc37c2c916e
xufeng01ww <a536874709@gmail.com>
2023-12-27 00:00:23 +0800

    2023/12/27

--- c_project/main.c
+++ c_project/main.c
@@ -7,7 +7,10 @@
 #define MAX_USERS 50
 #define NSUFTS 4
 #define NRANKS 13
+#define MAX_HAND_CARDS 5
 
+
+int currentCardIndex = 0;
 const char* sufts[NSUFTS] = { "方塊", "梅花", "紅心", "黑桃" };
 const char* ranks[NRANKS] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
@@ -15,13 +18,14 @@ typedef struct
 {
 	int suitIndex;
 	int rankIndex;
-	int value[2];
+	int value;
 }Card;
 
 typedef struct
 {
-	Card *hand;
-	int score;
+	Card hand[5];
+	int numCard;
+	int point;
 	int bet;
 }Player;
 
@@ -29,8 +33,10 @@ void initializeDeck(Card*); // 初始化牌組
 void shuffleDeck(Card*); // 洗牌
 void initializePlayer(Player*); // 每回合重製玩家數值
 void playerBet(Player*); // 玩家下注
-void firstRound(Player*); // 第一回合發牌
-void printBasicInfo(User);
+void deal(Player*, Card*); // 抽牌
+int checkHandCardsNumber(Player); // 檢查手牌是否超過5張
+void printBasicInfo(Player, int); // 印出目前玩家訊息
+int checkPoint(Player); // 檢查是否爆牌 回傳0(爆牌)、1(低於21)、2(等於21);
 
 int main()
 {
@@ -39,6 +45,7 @@ int main()
 	Card deck[NSUFTS * NRANKS];
 	char ans, flag;
 	int n;
+
 	srand((unsigned)time(NULL));
 	while (1)
 	{
@@ -52,39 +59,37 @@ int main()
 			// 判斷使用者選擇註冊、登入、退出
 			switch (ans)
 			{
-			case '1':
-				printf("請輸入使用者名稱: ");
-				scanf(" %s", user.username);
-				printf("請輸入密碼: ");
-				scanf(" %s", user.password);
-				if (loginUser(&user, MAX_USERS))
+				case '1':
+					printf("請輸入使用者名稱: ");
+					scanf(" %s", user.username);
+					printf("請輸入密碼: ");
+					scanf(" %s", user.password);
+					if (loginUser(&user, MAX_USERS))
+						flag = 1;
+					else
+						flag = 0;
+					break;
+				case '2':
+					regsiterUser(&user);
 					flag = 1;
-				else
-					flag = 0;
-				break;
-			case '2':
-				regsiterUser(&user);
-				flag = 1;
-				break;
-			case '0':
-				printf("...程式結束...\n");
-				Sleep(1000);
-
+					break;
+				case '0':
+					printf("...程式結束...\n");
+					Sleep(1000);
 			}
 		}
 
-		//顯示玩家訊息
+		// 顯示玩家訊息
 		printf("玩家名稱 %s\n", user.username);
 		printf("玩家剩餘金錢: %d\n", user.money);
 
 		printf("是否開始遊戲? (1為開始、0為上一步): ");
 		scanf(" %c", &ans);
 		if (ans == '0')
-		{
-			system("cls");
 			continue;
-		}
-			
+		system("cls");
+
+		// 遊戲開始
 		while (1)
 		{
 			// 初始化排組、玩家及洗牌
@@ -96,23 +101,83 @@ int main()
 			// 下注階段
 			playerBet(&player, user);
 
+			// 第一回合發牌
+			for (int i = 0; i < 2; i++)
+			{
+				deal(&player, &deck);
+				deal(&banker, &deck);
+			}
 
-
+			// 印出雙方目前持有的牌
+			printf("您目前的牌有: "); printBasicInfo(player, 1);
+			printf("莊家目前的牌有: "); printBasicInfo(banker, 0);
+			
 			
 
+			// 玩家回合
+			while (checkHandCardsNumber(player))
+			{
+				
+				char chice;
+				// 每次檢查是否爆牌
+				if (checkPoint(player) == 0)
+					break;
+				printf("\n拿牌(1)停牌(0): ");
+				scanf(" %c", &chice);
+				// 判斷是否正確輸入
+				if (chice != '1' && chice != '0')
+				{
+					printf("輸入錯誤,請重新輸入\n");
+					continue;
+				}
+				else
+				{
+					// 如果為1則拿牌,並印出目前有的牌
+					if (chice == '1')
+					{
+						deal(&player, &deck);
+						printf("您目前的牌有: "); printBasicInfo(player);
+					}
+					// 如果為0則玩家回合結束
+					else
+						break;
+				}
+				
+			}
 
 		}
 	}
-	
-	
-	
-
-	
 }
 
-void printBasicInfo(User user)
+void deal(Player* player, Card *deck)
 {
+	player->hand[player->numCard] = *(deck + currentCardIndex);
+	player->numCard++;
+	currentCardIndex++;
+}
+
+int checkHandCardsNumber(Player player)
+{
+	if (player.numCard < 5)
+		return 1;
+	else
+		return 0;
+}
+
+void printBasicInfo(Player player, int isPlayer)
+{
+	int i = 0;
+	// 莊家第一張牌為暗牌不顯示
+	if (isPlayer != 1)
+	{
+		i = 1;
+		printf("? ");
+	}
 	
+	for (;i < player.numCard; i++)
+		printf("%s%s ", sufts[player.hand->suitIndex], ranks[player.hand->rankIndex]);
+	printf('\n');
+	isPlayer ? printf("目前有%d張牌\n", player.numCard) : NULL;
 }
 
 void initializeDeck(Card *deck) 
@@ -124,20 +189,11 @@ void initializeDeck(Card *deck)
 			deck[i * NRANKS + j].suitIndex = i;
 			deck[i * NRANKS + j].rankIndex = j;
 			if (j == 0)
-			{
-				deck[i * NRANKS + j].value[0] = 1;
-				deck[i * NRANKS + j].value[1] = 11;
-			}
+				deck[i * NRANKS + j].value = 11;
 			else if (j > 9)
-			{
-				deck[i * NRANKS + j].value[0] = 10;
-				deck[i * NRANKS + j].value[1] = 10;
-			}
+				deck[i * NRANKS + j].value = 10;
 			else
-			{
-				deck[i * NRANKS + j].value[0] = j + 1;
-				deck[i * NRANKS + j].value[1] = j + 1;
-			}
+				deck[i * NRANKS + j].value = j + 1;
 		}
 	}
 }
@@ -157,8 +213,8 @@ void shuffleDeck(Card *deck)
 
 void initializePlayer(Player* player)
 {
-	player->hand = NULL;
-	player->score = 0;
+	player->numCard = 0;
+	player->point = 0;
 	player->bet = 0;
 }
 
@@ -168,14 +224,43 @@ void playerBet(Player* player, User user)
 	{
 		printf("請下注(目前持有金額為%d): ", user.money);
 		scanf("%d", &(player->bet));
-		if (player->bet > user.money)
+		// 輸入金額不符合則重新輸入
+		if (player->bet > user.money || player->bet <= 0)
 			printf("輸入金額錯誤,請重新輸入\n");
 		else
 			break;
 	}
 }
 
-void firstRound(Player* player, int *n)
+int checkPoint(Player player)
 {
-	
+	int count = 0;
+	player.point = 0;
+	// 計算目前點數
+	for (int i = 0; i < player.numCard; i++)
+	{
+		if (player.hand->rankIndex == 0)
+			count++;
+		player.point += player.hand->value;
+	}
+	while(count)
+	{
+		// 若A為11時點數超過21,則A為1
+		if (player.point > 21)
+		{
+			player.point -= 10;
+			count--;
+		}
+		// 若點數沒超過21則退出檢查
+		else
+			break;
+	}
+
+	// 檢查是否爆牌
+	if (player.point > 21)
+		return 0;
+	else if (player.point < 21)
+		return 1;
+	else
+		return 2;
 }
